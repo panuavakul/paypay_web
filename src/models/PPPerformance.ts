aa5b03229003cd6d1abebd8397765508b71465c0
Panu Avakul <balloon2258@gmail.com>
2020-06-02 21:33:58 +0900

    Add extra field to model

--- src/models/PPPerformance.ts
+++ src/models/PPPerformance.ts
@@ -1,28 +1,37 @@
 // This is the model for the performance
+// PP is just project unique labelled (PayPay)
 class PPPerformance {
   constructor({
     id,
+    date,
     employeeId,
     feedbackIds,
+    description,
     updatedAt,
     createdAt,
   }: {
     id: string;
+    date: Date;
     employeeId: string;
     feedbackIds: string[];
+    description: string;
     updatedAt: Date;
     createdAt: Date;
   }) {
     this.id = id;
+    this.date = date;
     this.employeeId = employeeId;
     this.feedbackIds = feedbackIds;
+    this.description = description;
     this.updatedAt = updatedAt;
     this.createdAt = createdAt;
   }
 
   id: string;
+  date: Date;
   employeeId: string;
   feedbackIds: string[];
+  description: string;
   updatedAt?: Date;
   createdAt?: Date;
 }
