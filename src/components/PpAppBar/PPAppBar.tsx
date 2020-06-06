994c7d4604574168d27c1efc318492e53595a079
Panu Avakul <balloon2258@gmail.com>
2020-06-06 20:03:47 +0900

    App bar deal with auth

--- src/components/PpAppBar/PPAppBar.tsx
+++ src/components/PpAppBar/PPAppBar.tsx
@@ -5,27 +5,45 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useSelector } from "react-redux";
 import { AppState } from "../../redux/store";
 import { useHistory } from "react-router-dom";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
+const authSelector = (state: AppState): boolean => {
+  const { isAdmin, userId } = state.common;
+  const hasAdminAuth = isAdmin === true;
+  const hasUserAuth = (userId ?? "").length > 0;
+  return hasAdminAuth || hasUserAuth;
+};
 
 const PPAppBar: React.SFC = props => {
   const classes = useStyles();
   const isLoading = useSelector((store: AppState) => store.common.isLoading);
+  const hasAuth = useSelector(authSelector);
   const history = useHistory();
 
   return (
     <AppBar className={classes.root} position="static">
       <Toolbar>
-        <Typography variant="h6" className={classes.leading}>
-          PayPay Challenge
-        </Typography>
-        <Button
-          className={classes.signinButton}
-          color="inherit"
-          onClick={() => {
-            history.push("/");
-          }}
-        >
-          Sign Out
-        </Button>
+        <Link to={`/`} style={{ color: "white", textDecoration: "none" }}>
+          <Typography
+            variant="h6"
+            className={classes.leading}
+            color={"inherit"}
+          >
+            PayPay Challenge
+          </Typography>
+        </Link>
+        {hasAuth && (
+          <Button
+            className={classes.signinButton}
+            color="inherit"
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            Sign Out
+          </Button>
+        )}
       </Toolbar>
       {isLoading && <LinearProgress />}
     </AppBar>
